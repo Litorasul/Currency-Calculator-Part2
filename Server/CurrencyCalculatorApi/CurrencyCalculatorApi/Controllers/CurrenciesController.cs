abebe3b57446e6458bc0aeb5d7ad4d841ab9a61a
Nikolay Kelov <n_kelov@yahoo.com>
2021-02-24 15:38:00 +0100

    Add CurrenciesController

---
+++ Server/CurrencyCalculatorApi/CurrencyCalculatorApi/Controllers/CurrenciesController.cs
@@ -0,0 +1,33 @@
+﻿namespace CurrencyCalculatorApi.Controllers
+{
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+    
+    using CurrencyCalculatorApi.Services;
+
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class CurrenciesController : ControllerBase
+    {
+        private readonly IFixerService service;
+
+        public CurrenciesController(IFixerService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Dictionary<string, string>>> GetAvailableCurrencies()
+        {
+            var currencies = await this.service.GetAvailableCurrenciesAsync();
+            if (!currencies.Success)
+            {
+                return this.NotFound();
+            }
+
+            return this.Ok(currencies.symbols);
+        }
+    }
+}
