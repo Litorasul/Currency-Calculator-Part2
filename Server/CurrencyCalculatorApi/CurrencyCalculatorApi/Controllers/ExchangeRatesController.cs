e31f26e6b7167ea778ecfc8b36deb7fff0d4a86f
Nikolay Kelov <n_kelov@yahoo.com>
2021-02-24 17:32:40 +0100

    Add ExchangeRatesController

---
+++ Server/CurrencyCalculatorApi/CurrencyCalculatorApi/Controllers/ExchangeRatesController.cs
@@ -0,0 +1,32 @@
+﻿namespace CurrencyCalculatorApi.Controllers
+{
+    using System.Collections.Generic;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    using CurrencyCalculatorApi.Services;
+    using CurrencyCalculatorApi.ViewModels;
+
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class ExchangeRatesController : ControllerBase
+    {
+        private readonly IDatabaseService service;
+
+        public ExchangeRatesController(IDatabaseService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<List<ExchangeRateViewModel>> GetRateForCurrency(string currencyCode)
+        {
+            var rates = this.service.GetAllRatesPerCurrency(currencyCode);
+            if (rates.Count < 1)
+            {
+                return this.NotFound();
+            }
+            return this.Ok(rates);
+        }
+    }
+}
